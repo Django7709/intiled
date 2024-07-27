9dbeaca06177e6085d953214b73c99d28c7f82ce
django <pitbull.09@mail.ru>
2024-07-27 15:17:25 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-}
+
+    var a = 1;
+    if(a >=0);
+        System.out.println("Число положительное");
